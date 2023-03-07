b8faea6a7bf4f51c2ec285def3e42fc07111c59f
c5ab <97299580+c5ab@users.noreply.github.com>
2023-03-07 20:50:23 +0530

    Add files via upload

---
+++ build/precache-manifest.585b227446ecd6959adab4adefb9e3f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694ca634db030975f847926c88447f4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ebf20563e7d7a18d444",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9ebf20563e7d7a18d444",
+    "url": "/static/js/2.b1ed7d04.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/static/js/2.b1ed7d04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451e8124e09b37a11685",
+    "url": "/static/js/main.fff07178.chunk.js"
+  },
+  {
+    "revision": "992c1d3a7787c5ab29a5",
+    "url": "/static/js/runtime-main.eeaa320a.js"
+  }
+]);
